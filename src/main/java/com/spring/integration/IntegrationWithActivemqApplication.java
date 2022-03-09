44c1b8c754dde92a294aa73d99b75144e9456a95
Praveen <abp.praveen@gmail.com>
2022-03-09 23:55:33 +0530

    Initial commit

---
+++ src/main/java/com/spring/integration/IntegrationWithActivemqApplication.java
@@ -0,0 +1,15 @@
+package com.spring.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationWithActivemqApplication {
+
+	public static void main(String[] args) {
+		
+		// Next example: JMS with microservices
+		SpringApplication.run(IntegrationWithActivemqApplication.class, args);
+	}
+
+}
